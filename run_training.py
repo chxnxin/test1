cbb7d4fcacffc1775c68da5f5b44e89e79bea5ac
erchenxin <erchenxinn@gmail.com>
2024-09-30 14:42:36 +0800

    update

--- run_training.py
+++ run_training.py
@@ -8,15 +8,14 @@ import torch.nn.functional as F
 import transformers
 import wandb
 import json
+import torch.nn as nn
+import math
 
 from dataset.dcase24 import get_training_set, get_test_set, get_eval_set
 from helpers.init import worker_init_fn
 from models.baseline import get_model
 from helpers.utils import mixstyle
 from helpers import nessi
-
-
-import random
 
 
 class PLModule(pl.LightningModule):
@@ -122,8 +121,6 @@ class PLModule(pl.LightningModule):
         :param batch_idx
         :return: loss to update model parameters
         """
-        #crietrion = torch.nn.CrossEntropyLoss()
-        
         x, files, labels, devices, cities = train_batch
         labels = labels.type(torch.LongTensor)
         labels = labels.to(self.device)
@@ -148,7 +145,7 @@ class PLModule(pl.LightningModule):
         x, files, labels, devices, cities = val_batch
 
         y_hat = self.forward(x)
-        lables = labels.type(torch.LongTensor)
+        labels = labels.type(torch.LongTensor)
         labels = labels.to(self.device)
         samples_loss = F.cross_entropy(y_hat, labels, reduction="none")
 
@@ -235,7 +232,7 @@ class PLModule(pl.LightningModule):
         # maximum memory allowance for parameters: 128 KB
         # baseline has 61148 parameters -> we can afford 16-bit precision
         # since 61148 * 16 bit ~ 122 kB
-
+ 
         # assure fp16
         self.model.half()
         x = self.mel_forward(x)
@@ -329,6 +326,101 @@ class PLModule(pl.LightningModule):
 
         return files, y_hat
 
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class ChannelGate(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+        super(ChannelGate, self).__init__()
+        self.gate_channels = gate_channels
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, int(gate_channels // reduction_ratio)),
+            nn.ReLU(),
+            nn.Linear(int(gate_channels // reduction_ratio), gate_channels)
+            )
+        self.pool_types = pool_types
+    def forward(self, x):
+        channel_att_sum = None
+        for pool_type in self.pool_types:
+            if pool_type=='avg':
+                avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( avg_pool )
+            elif pool_type=='max':
+                max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( max_pool )
+            elif pool_type=='lp':
+                lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( lp_pool )
+            elif pool_type=='lse':
+                # LSE pool only
+                lse_pool = logsumexp_2d(x)
+                channel_att_raw = self.mlp( lse_pool )
+
+            if channel_att_sum is None:
+                channel_att_sum = channel_att_raw
+            else:
+                channel_att_sum = channel_att_sum + channel_att_raw
+
+        scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+        return x * scale
+
+def logsumexp_2d(tensor):
+    tensor_flatten = tensor.view(tensor.size(0), tensor.size(1), -1)
+    s, _ = torch.max(tensor_flatten, dim=2, keepdim=True)
+    outputs = s + (tensor_flatten - s).exp().sum(dim=2, keepdim=True).log()
+    return outputs
+
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
+
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        kernel_size = 7
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=int((kernel_size-1) // 2), relu=False)
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = F.sigmoid(x_out) # broadcasting
+        return x * scale
+
+class cbam_module(nn.Module):
+    def __init__(self, gate_channels, reduction=16, pool_types=['avg', 'max'], no_spatial=False):
+        super(cbam_module, self).__init__()
+        self.ChannelGate = ChannelGate(gate_channels, reduction, pool_types)
+        self.no_spatial=no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate()
+
+    @staticmethod
+    def get_module_name():
+        return "cbam"
+    
+    def forward(self, x):
+        x_out = self.ChannelGate(x)
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x_out)
+        return x_out
 
 def train(config):
     # logging is done using wandb
@@ -370,7 +462,7 @@ def train(config):
     # on which kind of device(s) to train and possible callbacks
     trainer = pl.Trainer(max_epochs=config.n_epochs,
                          logger=wandb_logger,
-                         accelerator='gpu',
+                         accelerator='cpu',
                          devices=1,
                          precision=config.precision,
                          callbacks=[pl.callbacks.ModelCheckpoint(save_last=True)])
@@ -406,7 +498,7 @@ def evaluate(config):
     # load lightning module from checkpoint
     pl_module = PLModule.load_from_checkpoint(ckpt_file, config=config)
     trainer = pl.Trainer(logger=False,
-                         accelerator='gpu',
+                         accelerator='cpu',
                          devices=1,
                          precision=config.precision)
 
