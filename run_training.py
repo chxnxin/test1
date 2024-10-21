90464b8d5f0e0121695c30c0079f1ecb960ed38a
erchenxin <erchenxinn@gmail.com>
2024-10-21 17:03:23 +0800

    add cbam and simam

--- run_training.py
+++ run_training.py
@@ -10,7 +10,6 @@ import wandb
 import json
 import torch.nn as nn
 import math
-import os
 
 from dataset.dcase24 import get_training_set, get_test_set, get_eval_set
 from helpers.init import worker_init_fn
@@ -326,7 +325,115 @@ class PLModule(pl.LightningModule):
         y_hat = self.model(x)
 
         return files, y_hat
+
+class CNNWithCBAM(nn.Module):
+    def __init__(self):
+        super(CNNWithCBAM, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        
+        # Integrating CBAM after first and second convolution layers
+        self.cbam1 = cbam_module(gate_channels=64, reduction=16, pool_types=['avg', 'max'])
+        self.cbam2 = cbam_module(gate_channels=128, reduction=16, pool_types=['avg', 'max'])
+        
+        self.fc = nn.Linear(128 * 8 * 8, 10)  # Fully connected layer
     
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class ChannelGate(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
+        super(ChannelGate, self).__init__()
+        self.gate_channels = gate_channels
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(gate_channels, int(gate_channels // reduction_ratio)),
+            nn.ReLU(),
+            nn.Linear(int(gate_channels // reduction_ratio), gate_channels)
+            )
+        self.pool_types = pool_types
+    def forward(self, x):
+        channel_att_sum = None
+        for pool_type in self.pool_types:
+            if pool_type=='avg':
+                avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( avg_pool )
+            elif pool_type=='max':
+                max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( max_pool )
+            elif pool_type=='lp':
+                lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( lp_pool )
+            elif pool_type=='lse':
+                # LSE pool only
+                lse_pool = logsumexp_2d(x)
+                channel_att_raw = self.mlp( lse_pool )
+
+            if channel_att_sum is None:
+                channel_att_sum = channel_att_raw
+            else:
+                channel_att_sum = channel_att_sum + channel_att_raw
+
+        scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+        return x * scale
+
+def logsumexp_2d(tensor):
+    tensor_flatten = tensor.view(tensor.size(0), tensor.size(1), -1)
+    s, _ = torch.max(tensor_flatten, dim=2, keepdim=True)
+    outputs = s + (tensor_flatten - s).exp().sum(dim=2, keepdim=True).log()
+    return outputs
+
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
+
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        kernel_size = 7
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=int((kernel_size-1) // 2), relu=False)
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = F.sigmoid(x_out) # broadcasting
+        return x * scale
+
+class cbam_module(nn.Module):
+    def __init__(self, gate_channels, reduction=16, pool_types=['avg', 'max'], no_spatial=False):
+        super(cbam_module, self).__init__()
+        self.ChannelGate = ChannelGate(gate_channels, reduction, pool_types)
+        self.no_spatial=no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate()
+
+    @staticmethod
+    def get_module_name():
+        return "cbam"
+    
+    def forward(self, x):
+        x_out = self.ChannelGate(x)
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x_out)
+        return x_out
+
 class CNNWithSimAM(nn.Module): 
     def __init__(self):
         super(CNNWithSimAM, self).__init__()
@@ -367,20 +474,19 @@ class ChannelAttention(nn.Module):
         self.fc = nn.Linear(128 * 8 * 8, 10)  # Adjust dimensions as needed
 
 
-
     def forward(self, x):
         # First conv layer
         x = self.conv1(x)
         x = F.relu(x)
         x = self.simam1(x)  # Apply SimAM after the first convolution layer
-        x = self.channel_attention1(x)       # Apply Channel Attention
+        x = self.cbam1(x)  # Apply CBAM before convolution
         x = self.pool(x)  # Apply pooling
 
         # Second conv layer
         x = self.conv2(x)
         x = F.relu(x)
         x = self.simam2(x)  # Apply SimAM after the second convolution layer
-        x = self.channel_attention1(x)       # Apply Channel Attention
+        x = self.cbam2(x)  # Apply CBAM before convolution
         x = self.pool(x)  # Apply pooling again
 
         # Flatten and fully connected layer
@@ -429,19 +535,15 @@ def train(config):
     assert config.subset in {100, 50, 25, 10, 5}, "Specify an integer value in: {100, 50, 25, 10, 5} to use one of " \
                                                   "the given subsets."
     roll_samples = config.orig_sample_rate * config.roll_sec
-    # if os.name == "posix":
-    #     n_workers = 4
-    # else:
-    #     n_workers = 0
     train_dl = DataLoader(dataset=get_training_set(config.subset, roll=roll_samples),
-                        #   worker_init_fn=worker_init_fn,
-                          num_workers=config.num_workers,
+                          worker_init_fn=worker_init_fn,
+                          num_workers=1,
                           batch_size=config.batch_size,
                           shuffle=True)
 
     test_dl = DataLoader(dataset=get_test_set(),
-                        #  worker_init_fn=worker_init_fn,
-                          num_workers=config.num_workers,
+                         worker_init_fn=worker_init_fn,
+                         num_workers=config.num_workers,
                          batch_size=config.batch_size)
 
     # create pytorch lightening module
